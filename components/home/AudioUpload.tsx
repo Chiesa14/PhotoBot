b87711d6918c96b211b9529f2a0153c630b39a51
Chiesa14 <remychiesa14@gmail.com>
2024-01-01 08:38:54 +0200

    Starting audio upload

--- components/home/AudioUpload.tsx
+++ components/home/AudioUpload.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./audioUpload.styles";
+// import { Modal, Portal, Provider } from "react-native-paper";
+// import { Ionicons, FontAwesome } from "@expo/vector-icons";
+// import { COLORS, SIZES } from "../../constants";
 
 const AudioUpload = () => {
+  const [recording, setRecording] = useState(false);
+  const [sound, setSound] = useState();
+  const [audioUri, setAudioUri] = useState("");
+
+  
   return (
     <View>
-      <TouchableOpacity style={styles.uploadButton}>
+      <TouchableOpacity style={styles.uploadButton} activeOpacity={0.6}>
         <Text style={styles.uploadButtonText}>Upload Voice</Text>
       </TouchableOpacity>
     </View>
