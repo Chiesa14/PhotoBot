56db17a77af7e6c240c3d8d356932a402c58e216
Chiesa14 <remychiesa14@gmail.com>
2024-01-01 11:05:10 +0200

    recording audio

--- components/home/AudioUpload.tsx
+++ components/home/AudioUpload.tsx
@@ -1,20 +1,91 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
+import { Audio } from "expo-av";
+import { Ionicons } from "@expo/vector-icons";
 import { styles } from "./audioUpload.styles";
-// import { Modal, Portal, Provider } from "react-native-paper";
-// import { Ionicons, FontAwesome } from "@expo/vector-icons";
-// import { COLORS, SIZES } from "../../constants";
 
-const AudioUpload = () => {
-  const [recording, setRecording] = useState(false);
-  const [sound, setSound] = useState();
-  const [audioUri, setAudioUri] = useState("");
+interface RecordingData {
+  sound: Audio.Sound | null;
+  duration: string;
+  file: string | null;
+}
 
-  
+const AudioUpload: React.FC = () => {
+  const [recording, setRecording] = useState<Audio.Recording | undefined>();
+  const [recordings, setRecordings] = useState<RecordingData[]>([]);
+
+  const startRecording = async () => {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true,
+        });
+        const { recording } = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+        setRecording(recording);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const stopRecording = async () => {
+    if (recording) {
+      setRecording(undefined);
+
+      await recording.stopAndUnloadAsync();
+      const { sound, status } = await recording.createNewLoadedSoundAsync();
+      if (status.isLoaded) {
+        const { durationMillis } = status;
+        const newRecording: RecordingData = {
+          sound,
+          duration: getDurationFormatted(durationMillis),
+          file: recording?.getURI() || null,
+        };
+        setRecordings([newRecording]);
+      }
+    }
+  };
+  const getRecordingLines = () => {
+    return recordings.map((recordline, index) => {
+      return (
+        <View key={index}>
+          <Text>
+            AUdio Recorder| {recordline.duration}
+          </Text>
+          <TouchableOpacity onPress={clearRecord} activeOpacity={0.6}>
+            <Ionicons name="close" />
+          </TouchableOpacity>
+        </View>
+      );
+    });
+  };
+
+  const getDurationFormatted = (milliseconds: number) => {
+    const totalSeconds = Math.round(milliseconds / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  const clearRecord = () => {
+    setRecordings([]);
+  };
+
   return (
-    <View>
-      <TouchableOpacity style={styles.uploadButton} activeOpacity={0.6}>
-        <Text style={styles.uploadButtonText}>Upload Voice</Text>
+    <View style={styles.container}>
+      {getRecordingLines()}
+      <TouchableOpacity
+        activeOpacity={0.6}
+        onPress={recording ? stopRecording : startRecording}
+        style={styles.uploadButton}
+      >
+        <Text style={styles.uploadButtonText}>
+          {recording ? "Stop Recording" : "Start Recording"}
+        </Text>
       </TouchableOpacity>
     </View>
   );
--- components/home/audioUpload.styles.js
+++ components/home/audioUpload.styles.js
@@ -2,8 +2,11 @@ import { StyleSheet } from "react-native";
 import { SIZES, COLORS } from "../../constants";
 
 export const styles = StyleSheet.create({
+  container: {
+    marginTop: SIZES.small,
+  },
   uploadButton: {
-    marginTop: SIZES.xSmall,
+    marginTop: SIZES.xSmall - 2,
     backgroundColor: COLORS.green,
     height: SIZES.xxLarge,
     justifyContent: "center",
