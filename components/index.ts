5118256f0491f9f84faf36ff10b8e50b08240c48
Chiesa14 <remychiesa14@gmail.com>
2024-01-15 21:44:20 +0200

    slit edits

--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 import ImageUpload from "./home/ImageUpload";
 import AudioUpload from "./home/AudioUpload";
+import AudioList from "./chat/AudioLists";
 
-export { ImageUpload, AudioUpload };
+export { ImageUpload, AudioUpload, AudioList };
