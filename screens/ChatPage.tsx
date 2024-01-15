1589cc6a18d8955ea7143c0dcf816ef1904d4c6e
Chiesa14 <remychiesa14@gmail.com>
2024-01-15 21:44:11 +0200

    audio input

--- screens/ChatPage.tsx
+++ screens/ChatPage.tsx
@@ -1,24 +1,37 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useEffect, useRef, useState } from "react";
 import {
-  Text,
-  View,
-  Image,
   TouchableOpacity,
   BackHandler,
   ToastAndroid,
+  View,
+  Image,
+  Animated,
+  Easing,
 } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Audio } from "expo-av";
 import { SIZES } from "../constants";
 import styles from "./chatPage.styles";
+import { AudioList } from "../components";
+
+interface RecordingData {
+  sound: Audio.Sound | null;
+  duration: string;
+  file: string | null;
+  name: string;
+}
 
 const ChatPage = () => {
   const [imageUri, setImageUri] = useState<string | null>(null);
   const [audioUri, setAudioUri] = useState<string | null>(null);
   const [sound, setSound] = useState<Audio.Sound | null>(null);
+  const [recording, setRecording] = useState<Audio.Recording | undefined>();
+  const [recordings, setRecordings] = useState<RecordingData[]>([]);
+
   const backPressedOnceRef = useRef(false);
+  const fadeInAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     fetchData();
@@ -32,6 +45,24 @@ const ChatPage = () => {
       backHandler.remove();
     };
   }, [sound]);
+
+  useEffect(() => {
+    if (recording === undefined) {
+      Animated.timing(fadeInAnim, {
+        toValue: 1,
+        duration: 500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      Animated.timing(fadeInAnim, {
+        toValue: 0,
+        duration: 500,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }).start();
+    }
+  }, [recording, fadeInAnim]);
 
   const fetchData = async () => {
     try {
@@ -65,7 +96,47 @@ const ChatPage = () => {
 
     return true;
   };
+  const startRecording = async () => {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true,
+        });
+        const { recording } = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+        );
+        setRecording(recording);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const getDurationFormatted = (milliseconds: number) => {
+    const totalSeconds = Math.round(milliseconds / 1000);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
 
+  const stopRecording = async () => {
+    setRecording(undefined);
+
+    await recording!.stopAndUnloadAsync();
+    let allRecordings = [...recordings];
+    const { sound, status } = await recording!.createNewLoadedSoundAsync();
+    if (status.isLoaded) {
+      const { durationMillis } = status;
+      allRecordings.push({
+        sound,
+        duration: getDurationFormatted(durationMillis as number),
+        file: recording?.getURI() || null,
+        name: "user",
+      });
+    }
+    setRecordings(allRecordings);
+  };
   return (
     <SafeAreaView style={styles.container}>
       {imageUri && (
@@ -74,11 +145,41 @@ const ChatPage = () => {
           source={{ uri: imageUri }}
         />
       )}
-      <TouchableOpacity style={styles.audioIcon}>
-        <FontAwesome name="microphone" size={34} />
-      </TouchableOpacity>
+      <AudioList items={recordings} />
+      {recording === undefined ? (
+        <Animated.View
+          style={{
+            opacity: fadeInAnim,
+          }}
+        >
+          <TouchableOpacity
+            style={styles.audioIcon}
+            activeOpacity={0.6}
+            onPress={startRecording}
+          >
+            <FontAwesome name="microphone" size={20} />
+          </TouchableOpacity>
+        </Animated.View>
+      ) : (
+        <View style={styles.recordView}>
+          <TouchableOpacity style={styles.audioIcon} activeOpacity={0.6}>
+            <FontAwesome name="microphone" size={20} />
+          </TouchableOpacity>
+          <Image
+            style={{ height: 40 }}
+            source={require("/home/c_i__a/Desktop/studies/React-Native/PhotoBot/assets/record.gif")}
+          />
+          <TouchableOpacity>
+            <FontAwesome
+              style={{ marginRight: 10 }}
+              name="paper-plane"
+              size={20}
+              onPress={stopRecording}
+            />
+          </TouchableOpacity>
+        </View>
+      )}
     </SafeAreaView>
   );
 };
-
 export default ChatPage;
